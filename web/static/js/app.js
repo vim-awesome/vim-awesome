b72532325d399518a11fb4e2a8e6ccc3bbcf0042
Ben Alpert <spicyjalapeno@gmail.com>
2013-06-02 03:06:08 -0700

    Use React (and some Backbone) instead of Angular
    Summary: It's totally optional that we use this but I like React a lot.
    
    Test Plan: Loaded the homepage and saw both plugins listed.
    
    Reviewers: divad12, xymostech
    
    Reviewed By: divad12
    
    Differential Revision: http://phabricator.benalpert.com/D18

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,17 +1,137 @@
+/** @jsx React.DOM */
 (function() {
-'use strict';
-/*global window: false */
+"use strict";
 
-var TEMPLATE_URL = 'static/partials';
+var Sidebar = React.createClass({
+  render: function() {
+    var categories = _.map([
+      "Language",
+      "Syntax",
+      "Navigation",
+      "Movement",
+      "Buffer",
+      "Info",
+      "Integrations"
+    ], function(cat) {
+      return <li>
+        <a href="#/blah"><i class="icon-fighter-jet"></i> {cat}</a>
+      </li>;
+    });
 
-// Declare app level module which depends on filters, and services
-window.angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider
-      .when('', {templateUrl: TEMPLATE_URL + '/plugins_list.html', controller: 'PluginsList'})
-      .when('/view1', {templateUrl: TEMPLATE_URL + '/partial1.html', controller: 'MyCtrl1'})
-      .when('/view2', {templateUrl: TEMPLATE_URL + '/partial2.html', controller: 'MyCtrl2'})
-      .otherwise({redirectTo: ''});
-  }]);
+    return <div class="sidebar">
+      <h1 class="title">
+        <a href="#">
+          <span class="vim">Vim</span>Awesome
+        </a>
+      </h1>
+      <div class="tac">
+        <div class="subtitle">
+          <div class="line1">Awesome Vim plugins</div>
+          <div class="from">from</div>
+          <div class="line2">across the Universe</div>
+        </div>
+      </div>
+      <ul class="categories">{categories}</ul>
+    </div>;
+  }
+});
+
+var SearchBox = React.createClass({
+  handleKeyUp: React.autoBind(function() {
+    var input = this.refs.input.getDOMNode();
+    this.props.onInput(input.value);
+  }),
+
+  render: function() {
+    return <div class="search-container">
+      <i class="icon-search"></i>
+      <input type="text" class="search" placeholder="Search" ref="input"
+        onKeyUp={this.handleKeyUp} />
+    </div>;
+  }
+});
+
+var PluginList = React.createClass({
+  getInitialState: function() {
+    return {
+      plugins: []
+    };
+  },
+
+  componentDidMount: function() {
+    var component = this;
+    $.ajax({
+      url: "/api/plugins",
+      dataType: "json",
+      success: function(data) {
+        component.setState({plugins: data});
+      }
+    });
+  },
+
+  render: function() {
+    var query = this.props.searchQuery.toLowerCase();
+    var plugins = _.chain(this.state.plugins)
+      .filter(function(plugin) {
+        return plugin.name.toLowerCase().indexOf(query) !== -1;
+      })
+      .sortBy("github_stars")
+      .reverse()
+      .map(function(plugin) {
+        return <li class="plugin">
+          <div class="hover-bg"></div>
+          <div class="hover-buttons">
+            <a class="github-link" target="_blank" href={plugin.github_url}>
+              github link
+            </a>
+          </div>
+          <h3 class="plugin-name">
+            <a href={"#/plugin/" + plugin.name}>{plugin.name}</a>
+          </h3>
+          <p class="short-desc">{plugin.short_desc}</p>
+        </li>
+      })
+      .value();
+
+    return <div class="content row-fluid">
+      <ul class="plugins">{plugins}</ul>
+    </div>;
+  }
+});
+
+var Page = React.createClass({
+  getInitialState: function() {
+    return {searchQuery: ""};
+  },
+
+  onSearchInput: React.autoBind(function(query) {
+    this.setState({searchQuery: query});
+  }),
+
+  render: function() {
+    // TODO(alpert): Support multiple pages, not just the plugin list
+    return <div class="page-container">
+      <Sidebar />
+      <div>
+        <SearchBox onInput={this.onSearchInput} />
+        <PluginList searchQuery={this.state.searchQuery} />
+      </div>
+    </div>;
+  }
+});
+
+// TODO(alpert): Get rid of Backbone?
+var Router = Backbone.Router.extend({
+  routes: {
+    "": "home"
+  },
+
+  home: function() {
+    React.renderComponent(<Page />, document.body);
+  }
+});
+
+new Router();
+Backbone.history.start({pushState: true});
 
 })();
