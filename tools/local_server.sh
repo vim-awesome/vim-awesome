15925e8cb34f04a41b6066d71791effafeabcb56
David Hu <davidhu91@gmail.com>
2013-12-29 10:52:35 -0500

    fix local_server.sh script due to rethinkdb v1.11 regression
    Test Plan:
    Before this, whenever you kill your local_server.sh, the rethinkdb
    process would still be lingering around (and would then proceed to spin up your
    CPU in a vain attempt to clean up and die). Verified that now if the
    local_server.sh dies from ctrl-c or script error, rethinkdb will be killed by
    firing squad if after we politely asked for it to shut down it hasn't complied.
    
    Reviewers: spicyj, xymostech
    
    Reviewed By: spicyj
    
    Differential Revision: http://phabricator.benalpert.com/D124

--- tools/local_server.sh
+++ tools/local_server.sh
@@ -6,23 +6,24 @@ set -e
 function clean_up() {
   set +e
   kill 0
+
+  # Force kill any processes that haven't died after a while. Sometimes, but not
+  # always, RethinkDB v1.11.2 refuses to be killed by SIGTERM when backgrounded
+  # on OS X Mountain Lion.
+  ( sleep 2; kill -9 0 ) &
+
   exit
 }
 
 # Kill all child processes on script abort
 trap clean_up SIGTERM SIGINT ERR
 
+echo "Starting compass watch"
+bundle exec compass watch --config conf/compass.rb &
+
 echo "Starting rethinkdb"
 [ -d db/rethinkdb_data ] || rethinkdb create -d db/rethinkdb_data
 rethinkdb serve -d db/rethinkdb_data &
 
-echo "Starting compass watch"
-bundle exec compass watch --config conf/compass.rb &
-
 echo "Starting flask server"
 PYTHONPATH=. FLASK_CONFIG=../conf/flask_dev.py python web/server.py
-
-# Only exit on terminate or interrupt signal
-while true; do
-  sleep 1
-done
