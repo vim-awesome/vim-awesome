7c418b57b7e78c6cac41b27e3019aeb769fd53f8
David Hu <davidhu91@gmail.com>
2014-06-24 22:07:25 -0400

    First take at site footer
    Summary:
    About page still TODO. "RSS feed" link will just pop-up a thing that
    says something like "an rss feed would be awfully useful, but oh noes, we don't
    have one yet. however, we are open-sourced, and you're probably a coder, so ..."
    
    Also @xymostech I'm linking to your GitHub, is that what you want?
    
    Footer by itself:
    
    {F383}
    
    In context on homepage:
    
    {F384}
    
    Test Plan: in browser, on all the pages.
    
    Reviewers: xymostech, spicyj
    
    Reviewed By: spicyj
    
    Subscribers: xymostech
    
    Differential Revision: http://phabricator.benalpert.com/D184

--- web/static/js/app.jsx
+++ web/static/js/app.jsx
@@ -197,6 +197,51 @@ var Sidebar = React.createClass({
   }
 });
 
+// Site footer
+var Footer = React.createClass({
+  render: function() {
+    return <div className="site-footer">
+      <div className="first-row clearfix">
+        <div className="about-section">
+          <div className="about">
+            Vim Awesome is a directory of Vim plugins sourced from GitHub,
+            Vim.org, and user submissions. Plugin usage data is extracted from
+            dotfiles repos on GitHub.
+          </div>
+          <div className="credits">
+            Made with Vim by
+            {' '}<a target="_blank" href="https://twitter.com/divad12">
+              David Hu
+            </a>,
+            {' '}<a target="_blank" href="http://benalpert.com">
+              Ben Alpert
+            </a>, and
+            {' '}<a target="_blank" href="https://github.com/xymostech">
+              Emily Eisenberg
+            </a>.
+          </div>
+        </div>
+        <div className="github-section">
+          <a href="https://github.com/divad12/vim-awesome"
+              className="github-link">
+            Contribute
+            <i className="icon-github"></i>
+            on GitHub
+          </a>
+        </div>
+        <div className="links-section">
+          <ul className="links">
+            <li><a href="#">About</a></li>
+            <li><a href="/submit">Submit</a></li>
+            <li><a href="mailto:emacs@vimawesome.com">Email us</a></li>
+            <li><a href="#">RSS feed</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>;
+  }
+});
+
 var SearchBox = React.createClass({
   componentDidMount: function() {
     window.addEventListener("keyup", this.windowKeyUp, false);
@@ -1424,6 +1469,7 @@ var Page = React.createClass({
       <div className="content">
         {this.props.content}
         <WipNotice />
+        <Footer />
       </div>
     </div>;
   }
@@ -1473,7 +1519,7 @@ if (Backbone.history && Backbone.history._hasPushState) {
       return;
     }
     var href = $(this).attr("href");
-    var protocol = this.protocol + "//";
+    var protocol = this.protocol;
 
     // Only hijack URL to use Backbone router if it's relative (internal link)
     // and not a hash fragment.
