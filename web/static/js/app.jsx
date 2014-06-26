a04c117443b80f6dde4b7b23addebac505141e2d
David Hu <davidhu91@gmail.com>
2014-06-26 13:57:38 -0400

    Add GA tracking for single-page app

--- web/static/js/app.jsx
+++ web/static/js/app.jsx
@@ -1551,6 +1551,15 @@ var AboutPage = React.createClass({
 });
 
 var Page = React.createClass({
+  componentDidUpdate: function() {
+    // Google Analytics pageview tracking for single page app
+    // Thank you https://gist.github.com/daveaugustine/1771986#comment-958107
+    if (window.ga) {
+      var relativeUrl = window.location.pathname + window.location.search;
+      ga("send", "pageview", relativeUrl);
+    }
+  },
+
   render: function() {
     return <div className="page-container">
       <Sidebar />
