ebd0c858b923eb4b1bf947b03ae95b16ba7f4d89
David Hu <davidhu91@gmail.com>
2014-08-10 16:21:27 -0700

    Bandaid hack: restart RethinkDB (and server) daily
    Summary:
    ... because RethinkDB has been dying
    every few days for some reason.  I have tried updating RethinkDB's patch
    version, but the problem still seems to be there. There are no errors output to
    RethinkDB's log file when this happens.  This may or may not have to do with
    Rethinkdb gobbling up too much memory.  For now, we'll just restart the server
    and rethinkdb daily -- these restarts are graceful and quick and so shouldn't
    be too disruptive.
    
    In the future I should try experimenting with changing Rethinkdb
    cache sizes (though it looks like Rethinkdb is already using the minimum
    possible memory), or just ask on #rethinkdb.
    
    Test Plan:
    - ran script on server
    - it worked, things restarted correctly
    - hit vimawesome.com to ensure it still works
    - saw memory usage drop after restart according to `htop`
    
    Reviewers: spicyj, xymostech
    
    Differential Revision: http://phabricator.benalpert.com/D221

---
+++ tools/restart_rethinkdb_and_server.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+HOST="http://vimawesome.com"
+
+echo "Restarting RethinkDB"
+sudo service rethinkdb restart
+
+echo "Gracefully reloading server"
+kill -HUP $(cat $HOME/.gunicorn.pid)
+
+echo "Warming up caches"
+for i in {1..5}; do
+  curl -s "$HOST" > /dev/null
+  curl -s "$HOST/api/plugins?page=[1-10]" > /dev/null
+done
