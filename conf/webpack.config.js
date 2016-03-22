8a6179055d5372d1253686320f2a79e25938c760
Jordan Eldredge <jordan@jordaneldredge.com>
2016-03-22 11:53:20 -0400

    Add ESLint
    This includes the "recommended" rules both from ESLint itself and the react
    plugin.

--- conf/webpack.config.js
+++ conf/webpack.config.js
@@ -7,7 +7,7 @@ if (env === "development") {
   plugins = [
     new webpack.DefinePlugin({
       "process.env": {NODE_ENV: JSON.stringify("development")}
-    }),
+    })
   ];
 } else if (env === "production") {
   plugins = [
