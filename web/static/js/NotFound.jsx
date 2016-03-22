8d2aa031da7cdf34a622357e3828067152335507
denisinla <denis@reactiveventur.es>
2016-03-22 00:52:28 -0700

    #101 - Added route path and view for 404 message.

---
+++ web/static/js/NotFound.jsx
@@ -0,0 +1,16 @@
+/** @jsx React.DOM */
+
+"use strict";
+
+var React = require("react");
+
+var NotFound = React.createClass({
+  render: function() {
+    return <div className="not-found">
+        <h1>404</h1>
+        <p>The resource could not be found at this time, please <a href="/">try again</a>.</p>
+      </div>;
+  }
+});
+
+module.exports = NotFound;
