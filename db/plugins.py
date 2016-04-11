28fd7e5aa10be66368b895d902af534984a1cdc0
Jordan Eldredge <jordan@jordaneldredge.com>
2016-04-10 21:57:52 -0700

    Upgrade RethinkDB (fixes #94)
    To test this, I've loaded each of the web pages and tried all the actions
    I could think of. I also ran each of the tasks mentioned in `crontab` with no
    problems.
    
    Changelog here: https://github.com/rethinkdb/rethinkdb/releases?before=v1.16.1&after=v2.2.1
    
    Order of operations to deploy
    ----------------------------
    
    Upgrade the actual database package:
    
        apt-get install --only-upgrade rethinkdb=2.2.0
    
    Rebuild the index:
    
        rethinkdb index-rebuild
    
    Deploy this PR:
    
        make deploy

--- db/plugins.py
+++ db/plugins.py
@@ -555,12 +555,12 @@ def get_search_index():
 
     # Don't show plugin managers because they're not technically plugins, and
     # also our usage counts for them are not all accurate.
-    query = query.filter(r.all(
-        r.row['slug'] != 'vundle',
-        r.row['slug'] != 'neobundle-vim',
-        r.row['slug'] != 'neobundle-vim-back-to-december',
-        r.row['slug'] != 'pathogen-vim',
-    ))
+    query = query.filter(
+        (r.row['slug'] != 'vundle') &
+        (r.row['slug'] != 'neobundle-vim') &
+        (r.row['slug'] != 'neobundle-vim-back-to-december') &
+        (r.row['slug'] != 'pathogen-vim')
+    )
 
     plugins = map(to_json, query.run(r_conn()))
 
