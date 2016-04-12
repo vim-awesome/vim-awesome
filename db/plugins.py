e74b2bcde81a74130bed20a6697199bb070402a4
Jordan Eldredge <jordan@jordaneldredge.com>
2016-04-11 22:02:36 -0700

    Refactor ignoring plugin managers

--- db/plugins.py
+++ db/plugins.py
@@ -555,11 +555,13 @@ def get_search_index():
 
     # Don't show plugin managers because they're not technically plugins, and
     # also our usage counts for them are not all accurate.
+    plugin_manager_slugs = [
+        'vundle',
+        'neobundle-vim',
+        'neobundle-vim-back-to-december',
+        'pathogen-vim']
     query = query.filter(
-        (r.row['slug'] != 'vundle') &
-        (r.row['slug'] != 'neobundle-vim') &
-        (r.row['slug'] != 'neobundle-vim-back-to-december') &
-        (r.row['slug'] != 'pathogen-vim')
+        lambda row: r.expr(plugin_manager_slugs).contains(row['slug']).not_()
     )
 
     plugins = map(to_json, query.run(r_conn()))
