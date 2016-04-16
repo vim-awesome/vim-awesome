5efb445035277f7c389fdc04e2f55db35e529951
Jordan Eldredge <jordan@jordaneldredge.com>
2016-04-16 11:46:50 -0700

    Log deploys to Gitter

--- tools/notify_deploy.py
+++ tools/notify_deploy.py
@@ -1,37 +1,20 @@
-"""Sends a message to HipChat that we just deployed.
+"""Sends a message to Gitter that we just deployed.
 
 Args:
     $1: whoami
 """
 
 import sys
-
-import requests
-
-import secrets
+from util import log_to_gitter
 
 
-HIPCHAT_API_URL = 'https://api.hipchat.com/v1/rooms/message'
-
-
-def notify_hipchat(deployer, room_id):
+def notify_gitter(deployer):
     message = '%s just deployed to vimawesome.com' % deployer
-
-    payload = {
-        'auth_token': secrets.HIPCHAT_TOKEN,
-        'notify': 0,
-        'color': 'purple',
-        'from': 'Mr Monkey',
-        'room_id': room_id,
-        'message': message,
-        'message_format': 'text',
-    }
-
-    r = requests.post(HIPCHAT_API_URL, data=payload)
-    print 'Message sent to HipChat: %s\nResponse: %s' % (message, r.text)
+    log_to_gitter(message)
+    print 'Message sent to Gitter: %s' % message
 
 
 if __name__ == '__main__':
     deployer = sys.argv[1] if len(sys.argv) >= 2 else "vim-awesome server"
 
-    notify_hipchat(deployer, secrets.HIPCHAT_ROOM_ID)
+    notify_gitter(deployer)
