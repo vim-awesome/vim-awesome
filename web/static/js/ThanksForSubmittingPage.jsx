13743320e4d6564cb8e9e33b0f44f123ef0d51d0
Jordan Eldredge <jordan@jordaneldredge.com>
2016-04-26 10:35:21 -0700

    Split out Thank You page

---
+++ web/static/js/ThanksForSubmittingPage.jsx
@@ -0,0 +1,20 @@
+/** @jsx React.DOM */
+
+"use strict";
+
+var React = require("react");
+
+var ThanksForSubmittingPage = React.createClass({
+  render: function() {
+    return <div className="thanks-for-submitting-page">
+      <div className="thanks-box">
+        <h1>Thanks!</h1>
+        <p className="message">
+          Thank you for submitting a plugin! It will be reviewed shortly.
+        </p>
+      </div>
+    </div>;
+  }
+});
+
+module.exports = ThanksForSubmittingPage;
