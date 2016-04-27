5d23e71776145b1768b215e52034f8f06a9809d6
Jordan Eldredge <jordan@jordaneldredge.com>
2016-04-26 20:30:03 -0700

    Split out Page

---
+++ web/static/js/Page.jsx
@@ -0,0 +1,32 @@
+/** @jsx React.DOM */
+
+"use strict";
+
+var React = require("react");
+
+var Footer = require("./Footer.jsx");
+var Sidebar = require("./Sidebar.jsx");
+
+var Page = React.createClass({
+  componentDidUpdate: function() {
+    // Google Analytics pageview tracking for single page app
+    // Thank you https://gist.github.com/daveaugustine/1771986#comment-958107
+    var ga = window.ga;
+    if (ga) {
+      var relativeUrl = window.location.pathname + window.location.search;
+      ga("send", "pageview", relativeUrl);
+    }
+  },
+
+  render: function() {
+    return <div className="page-container">
+      <Sidebar query={this.props.query} />
+      <div className="content">
+        {this.props.activeRoute}
+        <Footer />
+      </div>
+    </div>;
+  }
+});
+
+module.exports = Page;
