cb25a867264a02b20eba5360d3b8aeba98a877a9
Jordan Eldredge <jordan@jordaneldredge.com>
2016-06-13 20:10:07 -0700

    Split out and test Pager

---
+++ web/static/js/__tests__/Pager-test.jsx
@@ -0,0 +1,16 @@
+"use strict"
+
+jest.unmock('../Pager.jsx');
+
+var React = require('react');
+var TestUtils = require('react-addons-test-utils');
+
+var Pager = require('../Pager.jsx');
+
+describe('Pager', function() {
+  it('renders without throwing', function() {
+    TestUtils.renderIntoDocument(
+      <Pager currentPage={1} totalPages={5} />
+    );
+  });
+});
--- web/static/js/app.jsx
+++ web/static/js/app.jsx
@@ -29,6 +29,7 @@ var Install = require("./Install.jsx");
 var Markdown = require("./Markdown.jsx");
 var NotFound = require("./NotFound.jsx");
 var Page = require("./Page.jsx");
+var Pager = require("./Pager.jsx");
 var Plaintext = require("./Plaintext.jsx");
 var Plugin = require("./Plugin.jsx");
 var SearchBox = require("./SearchBox.jsx");
@@ -61,94 +62,6 @@ var scrollToNode = function(domNode, context) {
     window.scrollTo(0, Math.max(0, elementTop - context));
   }
 };
-
-var Pager = React.createClass({
-  propTypes: {
-    currentPage: React.PropTypes.number.isRequired,
-    totalPages: React.PropTypes.number.isRequired
-  },
-
-  componentDidMount: function() {
-    window.addEventListener("keyup", this.onWindowKeyDown, false);
-  },
-
-  componentWillUnmount: function() {
-    window.removeEventListener("keyup", this.onWindowKeyDown, false);
-  },
-
-  onWindowKeyDown: function(e) {
-    var tag = e.target.tagName;
-    var key = e.keyCode;
-
-    if (tag !== "INPUT" && tag !== "TEXTAREA") {
-      if (key === KEYCODES.P) {
-        this.goToPrevPage();
-      } else if (key === KEYCODES.N) {
-        this.goToNextPage();
-      }
-    }
-  },
-
-  goToPage: function(page) {
-    var newPage = _.clamp(page, 1, this.props.totalPages);
-    this.props.onPageChange(newPage);
-  },
-
-  goToPrevPage: function() {
-    this.goToPage(this.props.currentPage - 1);
-  },
-
-  goToNextPage: function() {
-    this.goToPage(this.props.currentPage + 1);
-  },
-
-  onPrevClick: function(e) {
-    e.preventDefault();
-    this.goToPrevPage();
-  },
-
-  onNextClick: function(e) {
-    e.preventDefault();
-    this.goToNextPage();
-  },
-
-  render: function() {
-    var currentPage = this.props.currentPage;
-    var totalPages = this.props.totalPages;
-
-    if (totalPages <= 1) {
-      return <div />;
-    }
-
-    // TODO(david): Have buttons for page numbers, including first page, last
-    //     page, and current page.
-    return <div className="pagination">
-      <ul>
-        {currentPage > 1 &&
-          <li>
-            <a className="pager-button prev-page-button" href="#"
-                onClick={this.onPrevClick}>
-              {"\u2190"}<code>P</code>
-            </a>
-          </li>
-        }
-        <li>
-          <a className="page-number">Page {currentPage} of {totalPages}</a>
-        </li>
-        {currentPage < totalPages &&
-          <li>
-            <a className="pager-button next-page-button" href="#"
-                onClick={this.onNextClick}>
-              <code>N</code> Next page
-              <span className="right-arrow">{"\u2192"}</span>
-            </a>
-          </li>
-        }
-      </ul>
-    </div>;
-  }
-});
-
 
 var PluginList = React.createClass({
   getInitialState: function() {
